6e5815462a124c83659ba23ddbf5fe308ca751fe
Ysv0260 <ysv0260.autuni.ac.nz>
2020-09-12 22:03:10 +1200

    Android shit

---
+++ Silent turtles V1 10-09-2020/Assets/test.cs
@@ -0,0 +1,21 @@
+﻿using PedometerU.Tests;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+
+    private StepCounter sc;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
