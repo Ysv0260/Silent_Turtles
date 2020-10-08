d50d46b86d68c5ff79ac469219cd6464c0286ca0
Ysv0260 <ysv0260.autuni.ac.nz>
2020-10-09 01:09:34 +1300

    getting sound working though all scenes
    Saving is still a bit wakky but is kinda working
    
    sound is working :+1:

---
+++ Silent turtles V1 10-09-2020/Assets/Scripts/Scriptstotal.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scriptstotal : MonoBehaviour
+{
+
+    //scene change 
+
+    public void NextSceneP1()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        print("Forward one scene");
+    }
+    public void NextSceneP2()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+        print("Forward two scene");
+    }
+    public void BackSceneP1()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        print("Backwards one scenes");
+        
+    }
+   
+    public void BackSceneP2()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+        print("Backwards two scenes");
+    }
+     public void Exit()
+    {
+        print("1");
+        Application.Quit();
+        print("2");
+    }
+
+
+    //Sound
+
+
+    public void MuteVolume()
+    {
+        AudioListener.pause = !AudioListener.pause;
+        print("Off volume worked");
+    }
+
+    public void Fullvolume()
+    {
+        AudioListener.pause = false;
+        print("On volume worked");
+    }
+
+
+    public AudioSource AudioSource;
+    public float musicVolume = 1f;
+
+ 
+
+    
+    public void UpdateVolume(float volume)
+    {
+        AudioListener.volume = volume;
+        if (volume <= 0.1f)
+        {
+            print("volume is at low = " + volume);
+        }
+        else if (volume <= 0.5f)
+        {
+            print("volume is at medium = " + volume);
+        }
+        else if (volume <= 1)
+        {
+            print("volume is at high = " + volume);
+        }
+        else
+        {
+            print("volume" + volume);
+        }
+    }
+
+   
+
+}   
