96afe25ac3f5c20c2bb75daac99b2b305c58ea1e
SJ-47 <stjohnlyon2000@gmail.com>
2020-10-21 13:41:17 +1200

    Update

---
+++ Silent turtles V1 10-09-2020/Assets/Scripts/ShopControl.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopControl : MonoBehaviour
+{
+    public GameObject objectToDisable;
+    public static bool isDisabled = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDisabled)
+        
+                objectToDisable.SetActive(false);
+            else
+                objectToDisable.SetActive(true);
+    }
+}
